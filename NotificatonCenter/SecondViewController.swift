f56624d15ce0238760652949fe9faf4c466162b4
Salin Chhun <salinchhun123079@gmail.com>
2023-09-29 13:48:03 +0700

    Initial Commit

---
+++ NotificatonCenter/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  NotificatonCenter
+//
+//  Created by PVH_002 on 29/9/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    let notificationCenter = NotificationCenter.default
+    let notificationName = Notification.Name("string")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        notificationCenter.addObserver(self, selector: #selector(changeBg(notification: )), name: notificationName, object: nil)
+    }
+    
+    deinit {
+        notificationCenter.removeObserver(self, name: notificationName, object: nil)
+    }
+    
+    @objc func changeBg(notification: NSNotification) {
+        view.backgroundColor = notification.userInfo?["bg"] as! UIColor
+    }
+
+
+}
